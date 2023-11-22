5133211dda59405a7eceec5810249a9a7e6db847
Bratah123 <brandonnguyen3301@gmail.com>
2023-11-21 16:21:34 -0800

    FEAT: Add rudimentary methods

--- src/db/orange_db.py
+++ src/db/orange_db.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with CSUFTheOnion. If not, see <https://www.gnu.org/licenses/>.
 import sqlite3
+from db.article import Article
 
 DATABASE_NAME = "orange.db"
 
@@ -28,4 +29,29 @@ class OrangeDB:
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.con.close()
+
+    def get_articles_by_page(self, page_num) -> list[Article]:
+        """
+        Returns a list of articles on a given page.
+        Page 1 is the first 10 entries, page 2 is the next 10, etc.
+        """
+        cur = self.con.cursor()
+        cur.execute("SELECT * FROM articles ORDER BY id DESC LIMIT 10 OFFSET ?", (page_num * 10,))
+        articles = cur.fetchall()
+        cur.close()
+
+        article_objects = []
+
+        for article in articles:
+            article_objects.append(Article(article[1], article[2], article[3], article[4]))
+
+        return article_objects
+    
+    def get_article_by_id(self, article_id) -> Article:
+        cur = self.con.cursor()
+        cur.execute("SELECT * FROM articles WHERE id=?", (article_id,))
+        article = cur.fetchone()
+        cur.close()
+
+        return Article(article[1], article[2], article[3], article[4])
 
